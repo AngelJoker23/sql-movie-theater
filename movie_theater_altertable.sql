17ce07d94b0de7513c06d01a79d9fd2a4253d40c
AngelJoker23 <127627388+AngelJoker23@users.noreply.github.com>
2023-07-03 12:10:15 -0500

    Add files via upload

---
+++ movie_theater_altertable.sql
@@ -0,0 +1,11 @@
+alter table customers
+add membership varchar (150);
+
+delete from customers
+where number_purchased;
+
+drop table offers;
+
+alter table customers
+delete from customers
+where purchased_time; 
